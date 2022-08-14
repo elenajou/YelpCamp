15fb0768c7b5c8374f8edb2b24ba2dfc86d0f371
Elena Jou <ejouluo@hotmail.com>
2022-08-14 19:10:30 -0400

    Changes for authorization middleware

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const Campground = require('./models/campground');
 const methodOverride = require('method-override');
-
-const app = express();
+const session = require('express-session')
+const flash = require('connect-flash')
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users');
 
 mongoose.connect('mongodb://localhost:27017/YelpCamp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+const app = express();
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
@@ -24,47 +32,60 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));//parses the body from POST
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'Thisshouldbeabettersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig))
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+//use LocalStrategy and authenticate it with the User model
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());//store user in session
+passport.deserializeUser(User.deserializeUser());//remove user from session
+
+app.use((req, res, next) => {
+    //console.log(req.session);
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+// app.get('/fakeUser', async (req, res) => {
+//     const user = new User({email: 'yelpcamp@gmail.com', username: 'yelpcamp3'})
+//     const newUser = await User.register(user, 'chickennugget');
+//     res.send(newUser);
+// })
+
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+app.use('/', userRoutes);
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds });
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
 });
 
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
+app.use((err, req, res, next) => {
+    const {statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh no, Something went wrong!';
+    res.status(statusCode).render('error', { err });
 });
-
-app.post('/campgrounds', async(req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-});
-
-app.get('/campgrounds/:id', async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show', { campground });
-});
-
-app.get('/campgrounds/:id/edit', async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', { campground });
-});
-
-app.put('/campgrounds/:id', async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
-    res.redirect(`/campgrounds/${campground._id}`)
-})
-
-app.delete('/campgrounds/:id', async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds')
-})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async() => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random()* 20 + 10);
         const camp = new Campground({ 
+            author: '62f708e4f10ca506f6add9f9',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
